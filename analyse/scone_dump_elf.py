17d02c994acae08fc192b3b78a51f593755bebcd
Maurice Bailleu <m.bailleu@sms.ed.ac.uk>
2018-03-22 15:42:13 +0100

    Added path checks

--- analyse/scone_dump_elf.py
+++ analyse/scone_dump_elf.py
@@ -1,5 +1,6 @@
 #/usr/bin/env python3
 
+import os
 import argparse
 import subprocess
 from typing import List
@@ -11,13 +12,16 @@ def exec_docker(args: List[str]):
 
 def dump(container_name: str, executable: str, output: str):
     dump_file = "/tmp/scone_dump_elf"
-    exec_docker(["cp", executable, container_name+":/tmp/"+executable])
+    exec_file = "/tmp/"+os.path.basename(executable)
+    executable = os.path.abspath(executable)
+    exec_docker(["cp", executable, container_name+":"+exec_file])
     gdb = ["exec", container_name, "scone-gdb", "-nx", "--batch"]
     for cmd in ["break main", "run", "continue", "shell cp $SCONE_DUMP_DEBUG "+dump_file+"; exit"]:
         gdb.extend(["-ex". cmd])
+    gdb.append(exec_file)
     exec_docker(gdb)
     exec_docker(["cp", container_name+":"+dump_file, output])
-    exec_docker(["exec", container_name, "rm", "/tmp/"+executable, dump_file])
+    exec_docker(["exec", container_name, "rm", exec_file, dump_file])
 
 def main():
     parser = argparse.ArgumentParser(description="Dumps the encalve ELF from Scone")
