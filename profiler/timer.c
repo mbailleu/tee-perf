b8395339053f6f7eb8ad4f0735d8c84eb4f28514
Maurice Bailleu <M.Bailleu@web.de>
2018-04-23 15:48:26 +0100

    Timer app more functional

--- profiler/timer.c
+++ profiler/timer.c
@@ -78,7 +78,7 @@ static void unmap_shared_memory() {
 	}
 }
 
-static pid_t start_other(char * program, char ** args, char ** envp, int fd) {
+static pid_t start_other(char const * program, char ** args, char ** envp, int fd) {
 	pid_t pid;
 	if ((pid = fork()) == 0) {
 		size_t sz = 0;
@@ -100,7 +100,7 @@ static pid_t start_other(char * program, char ** args, char ** envp, int fd) {
 int softexit = 0;
 #endif
 
-static __attribute__((hot)) void * update_clock(void * ptr) {
+static void * update_clock(void * ptr) {
 	//struct timespec t;
 	uint64_t sec = 0;
 	uint64_t nsec = 0;
@@ -127,19 +127,75 @@ static __attribute__((hot)) void * update_clock(void * ptr) {
 	}
 }
 
-int main(int argc, char ** argv, char ** envp) {
-	if (argc < 3) {
-		printf("%s needs 2 arguments, filename and different program\n", argv[0]);
-		return 1;
-	}
-	char const * shm_name = argv[1];
+void print_usage(char * app_name) {
+	printf("Usage: %s [-h] log_filename size_in_byte app [app argument(s)]\n", app_name);
+}
 
+struct Args {
+	char const * self_name;
+	char const * shm_name;
+	size_t shm_size;
+	char const * app_name;
+	char ** app_args;
+};
+
+struct Args parse_args(int argc, char ** argv) {
+	for (size_t i = 0; i < argc; ++i) {
+		if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")) {
+			print_usage(argv[0]);
+			printf("\n"
+		       		"Starts to be profiled application and provides a monotonic clock to the application.\n"
+		       		"It also writes the profiler values to the log file\n"
+		       		"\n"
+		       		"positional arguments:\n"
+		       		"  log_filename          File to which the log should be written\n"
+		       		"  size_in_byte          Size of the log file to be created\n"
+				"                        supports suffixes k,M,G,T,P,E\n"
+		       		"  app                   Application to be started\n"
+		       		"\n"
+		       		"optional arguments:\n"
+		       		"  -h, --help            show his help message and exit\n"
+		       		"  app arguments         arguments which should be passed on the application\n");
+			exit(0);
+		}
+	}
+	if (argc < 3) {
+		print_usage(argv[0]);
+		exit((argc == 1) ? 0 : 1);
+	}
+	size_t size = 0;
+	{
+		size_t i = 0;
+		char c;
+		while((c = argv[2][i])) {
+			if (c >= '0' && c <= '9') {
+				size = size * 10 + (c - '0');
+				continue;
+			}
+			switch(c) {
+				case 'k':	size <<= 10; break;
+				case 'M':	size <<= 20; break;
+				case 'G':	size <<= 30; break;
+				case 'T':	size <<= 40; break;
+				case 'P':	size <<= 50; break;
+				case 'E':	size <<= 60; break;
+				default: 	print_error("unsupported symbol: %c in the size parameter\n", c);
+							exit(1);
+			}
+		}
+	}
+	struct Args res = {argv[0], argv[1], size, argv[3], argv + 3};
+	return res;
+}
+
+int main(int argc, char ** argv, char ** envp) {
+	struct Args args = parse_args(argc, argv);
 	int ret;
-	if ((ret = create_shared_memory(shm_name, __PROFILER_SHM_SIZE__))) {
+	if ((ret = create_shared_memory(args.shm_name, args.shm_size))) {
 		return ret;
 	}
 
-	pid_t child = start_other(argv[2], argv + 2, envp, shm_fd);
+	pid_t child = start_other(args.app_name, args.app_args, envp, shm_fd);
  	pthread_t clock;
 	if ((ret = pthread_create(&clock, NULL, update_clock, NULL)) != 0) {
 		print_error("Could not create clock thread\n");
@@ -151,6 +207,7 @@ int main(int argc, char ** argv, char ** envp) {
 		print_error("Child exited with code: %d\n", ret);
 	}
 #if !defined(SOFTEXIT)
+	//Let the OS clean up
 	return 0;
 #else
 	softexit = 1;
